e95855c0a385009ce07d941e5cafef6408dce48d
Hristiyan Ivanov <civanov.me@gmail.com>
2021-09-16 08:38:42 +0300

    fix: typo in Application.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./scss/app.scss";
-import Application from "./js/Appication";
+import Application from "./js/Application";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
